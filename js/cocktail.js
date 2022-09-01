fb99a7d6db131512b02c3c7e8f7d030eb4fc088d
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-09-01 20:21:29 +0600

    cocktail db show details

--- js/cocktail.js
+++ js/cocktail.js
@@ -1,4 +1,5 @@
 document.getElementById('search-btn').addEventListener('click',()=>{
+  toggleSpinner(true);
   const searchField = document.getElementById('search-field').value;
   loadData(searchField);
 });
@@ -6,6 +7,7 @@ document.getElementById('search-btn').addEventListener('click',()=>{
 document.getElementById('search-field').addEventListener("keypress",(e) => {
   const searchField = document.getElementById('search-field').value;
   if(e.key === 'Enter'){
+    toggleSpinner(true);
     loadData(searchField);
   }
 })
@@ -18,12 +20,13 @@ const loadData = id => {
 
 
 const displayData = data => {
-  console.log(data);
+  // console.log(data);
   const cardContainer = document.getElementById('card-container');
   cardContainer.textContent = '';
   const nothingFound = document.getElementById('nothing-found');
   if(data === null){
     nothingFound.classList.remove('d-none');
+    toggleSpinner(false);
     return;
   }
   else{
@@ -33,7 +36,7 @@ const displayData = data => {
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
-    <div class="card h-100">
+    <div class="card h-100" onclick="showDetails(${element.idDrink})" data-bs-toggle="modal" data-bs-target="#exampleModal">
         <img src="${element.strDrinkThumb}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${element.strDrink}</h5>
@@ -43,4 +46,33 @@ const displayData = data => {
     `;
     cardContainer.appendChild(div);
   });
-}
+  toggleSpinner(false);
+};
+
+  // spinner 
+  const toggleSpinner = isTrue => {
+    const spinner = document.getElementById('spinner');
+    if(isTrue === true){
+      spinner.classList.remove('d-none');
+    }
+    else{
+      spinner.classList.add('d-none');
+    }
+  };
+
+
+  const showDetails = id =>{
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then(res => res.json())
+    .then(data => displayDetails(data.drinks[0]))
+  };
+
+  const displayDetails = data =>{
+    console.log(data);
+    const modalTitle = document.getElementById('exampleModalLabel');
+    modalTitle.innerText = data.strDrink;
+    const modalBody = document.getElementById('details-modal-body');
+    modalBody.innerHTML = `
+    <img src="${data.strDrinkThumb}" class="card-img-top" alt="...">
+    `;
+  };
